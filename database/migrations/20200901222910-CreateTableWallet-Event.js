f71b4919c6aad6a0b0f1f14477361c2a6c134d81
Meisze Yau <meiszey@gmail.com>
2020-09-01 18:35:23 -0400

    Added all enum types and tables except for entity_trust_log, entity_trust, and transfer_audit

---
+++ database/migrations/20200901222910-CreateTableWallet-Event.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+/**
+  * We receive the dbmigrate dependency from dbmigrate initially.
+  * This enables us to not have to rely on NODE_PATH.
+  */
+exports.setup = function(options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = function(db) {
+  return db.createTable('wallet_event', {
+    id: { type: 'int', primaryKey: true, autoincrement: true },
+    entity_id: { type: 'int', notNull: true },
+    type: { type: 'wallet_event_type', notNull: true },
+    created_at: {
+      type: 'timestamp',
+      notNull: true,
+      defaultValue: new String('now()'),
+    },
+  });
+};
+
+exports.down = function(db) {
+  return db.dropTable('wallet_event');
+};
+
+exports._meta = {
+  "version": 1
+};
