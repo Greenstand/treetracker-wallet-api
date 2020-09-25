9ba84c808935a64ca13882337696fe793099db02
deanchen <antlet2004@gmail.com>
2020-09-26 07:55:46 +0800

    Added:command testWatch for developing; Fixed: bug in test

--- server/models/TrustModel.spec.js
+++ server/models/TrustModel.spec.js
@@ -20,6 +20,7 @@ describe("TrustModel", () => {
     sinon.stub(TrustRepository.prototype, "get").returns([{a:1}]);
     const trust_relationships = await trustModel.getTrustModel().get();
     expect(trust_relationships).lengthOf(1);
+    TrustRepository.prototype.get.restore();
   });
 
 
@@ -41,6 +42,8 @@ describe("TrustModel", () => {
       sinon.stub(EntityRepository.prototype, "getEntityByWalletName").returns([{id:1}]);
       sinon.stub(TrustRepository.prototype, "create");
       await trustModel.request("send", "test");
+      EntityRepository.prototype.getEntityByWalletName.restore();
+      TrustRepository.prototype.create.restore();
     });
   });
 
@@ -51,6 +54,8 @@ describe("TrustModel", () => {
       sinon.stub(TrustRepository.prototype, "update");
       const trustRelationshipId = 1;
       await trustModel.accept(trustRelationshipId);
+      TrustRepository.prototype.getById.restore();
+      TrustRepository.prototype.update.restore();
     });
   });
 
