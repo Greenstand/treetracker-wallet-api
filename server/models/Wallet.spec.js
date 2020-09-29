d1fda0862934517666ea9c88fb9ad1766fb19b26
deanchen <antlet2004@gmail.com>
2020-09-29 10:34:56 +0800

    Refine: wallet tests;

--- server/models/Wallet.spec.js
+++ server/models/Wallet.spec.js
@@ -9,14 +9,21 @@ const HttpError = require("../utils/HttpError");
 
 describe("Wallet", () => {
   let walletService;
+  let wallet;
+  const walletObject = {
+    id:1, 
+    salt:"salet",
+    password: "testPasswordHash",
+  };
 
   beforeEach(() => {
+    wallet = new Wallet(1);
     walletService = new WalletService();
   })
 
   it("authorize() with empty parameters should get 400 error", async () => {
     sinon.stub(WalletRepository.prototype, "getByName").resolves({id:1});
-    const wallet = await walletService.getByName("test");
+    wallet = await walletService.getByName("test");
     expect(wallet).instanceOf(Wallet);
     await jestExpect(async () => {
       await wallet.authorize(undefined);
@@ -35,18 +42,10 @@ describe("Wallet", () => {
 
 
   it("authorize() with correct wallet, password, should return a wallet object", async () => {
-    const walletObject = {
-      id:1, 
-      salt:"salet",
-      password: "testPasswordHash",
-    };
-    sinon.stub(WalletRepository.prototype, "getByName").resolves(walletObject);
     sinon.stub(WalletRepository.prototype, "getById").resolves(walletObject);
     sinon.stub(Wallet, "sha512").returns("testPasswordHash");
-    const wallet = await walletService.getByName("testWallet");
     const walletObjectResult = await wallet.authorize("testPassword");
     expect(walletObjectResult).property("id").eq(1);
-    WalletRepository.prototype.getByName.restore();
     WalletRepository.prototype.getById.restore();
     Wallet.sha512.restore();
   });
@@ -54,7 +53,6 @@ describe("Wallet", () => {
   it("get trust_relationships", async () => {
     sinon.stub(TrustRepository.prototype, "get").returns([{a:1}]);
     sinon.stub(WalletRepository.prototype, "getById").resolves({id:1});
-    const wallet = await walletService.getById(-1);
     const trust_relationships = await wallet.getTrustRelationships();
     expect(trust_relationships).lengthOf(1);
     WalletRepository.prototype.getById.restore();
