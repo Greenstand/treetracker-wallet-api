e3705e20c8e7e9db580a0c9d9b9d51b9afb6128f
deanchen <antlet2004@gmail.com>
2020-10-09 10:10:53 +0800

    Added: test for try to transfer token which do not belongs to wallet

--- server/routes/transferRouter.spec.js
+++ server/routes/transferRouter.spec.js
@@ -11,6 +11,7 @@ const JWTService = require("../services/JWTService");
 const HttpError = require("../utils/HttpError");
 const Token = require("../models/Token");
 const TokenService = require("../services/TokenService");
+const Wallet = require("../models/Wallet");
 
 describe("authRouter", () => {
   let app;
@@ -23,11 +24,6 @@ describe("authRouter", () => {
     sinon.stub(JWTService.prototype, "verify").returns({
       id: walletLogin.id,
     });
-    sinon.stub(WalletService.prototype, "getByName");
-    sinon.stub(WalletService.prototype, "getById").resolves({
-      transfer: () => {},
-    });
-    sinon.stub(TokenService.prototype, "getByUUID").resolves(new Token(1));
     app = express();
     app.use(bodyParser.urlencoded({ extended: false })); // parse application/x-www-form-urlencoded
     app.use(bodyParser.json()); // parse application/json
@@ -59,6 +55,75 @@ describe("authRouter", () => {
       });
     expect(res).property("statusCode").eq(422);
     expect(res.body.message[0].message).match(/sender.*required/);
+  });
+
+  it("all parameters fine, should return 201", async () => {
+    sinon.stub(WalletService.prototype, "getByName").resolves(new Wallet(1));
+    sinon.stub(WalletService.prototype, "getById").resolves({
+      transfer: () => {},
+    });
+    sinon.stub(TokenService.prototype,"getByUUID").resolves(new Token({
+      id: 1,
+      entity_id: 1,
+    }));
+    const res = await request(app)
+      .post("/")
+      .send({
+        tokens: ["1"],
+        sender_wallet: "ssss",
+        receiver_wallet: "ssss",
+      });
+    expect(res).property("statusCode").eq(201);
+  });
+
+  it("all parameters fine, but no trust relationship, should return 202", async () => {
+    sinon.stub(WalletService.prototype, "getByName").resolves(new Wallet(1));
+    sinon.stub(WalletService.prototype, "getById").resolves({
+      transfer: () => {},
+    });
+    sinon.stub(TokenService.prototype, "getByUUID").resolves(new Token({
+      id:1,
+      entity_id: 1,
+    }));
+    WalletService.prototype.getById.restore();    
+    sinon.stub(WalletService.prototype, "getById").resolves({
+      transfer: async () => {
+        throw new HttpError(202);
+      },
+    });
+    const res = await request(app)
+      .post("/")
+      .send({
+        tokens: ["1"],
+        sender_wallet: "ssss",
+        receiver_wallet: "ssss",
+      });
+    expect(res).property("statusCode").eq(202);
+  });
+
+  it("Given token uuid do not belongs to sender wallet, should throw 403", async () => {
+    sinon.stub(WalletService.prototype, "getByName").resolves(new Wallet(1));
+    sinon.stub(WalletService.prototype, "getById").resolves({
+      transfer: () => {},
+    });
+    sinon.stub(TokenService.prototype, "getByUUID").resolves(new Token({
+      id:1,
+      entity_id: 2,
+    }));
+    WalletService.prototype.getById.restore();    
+    sinon.stub(WalletService.prototype, "getById").resolves({
+      transfer: async () => {
+        throw new HttpError(202);
+      },
+    });
+    const res = await request(app)
+      .post("/")
+      .send({
+        tokens: ["1"],
+        sender_wallet: "ssss",
+        receiver_wallet: "ssss",
+      });
+    expect(res).property("statusCode").eq(403);
   });
 
 
