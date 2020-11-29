106fc06b900c044d64aaa7865c9babe729adff03
Zaven <matthew.fullstackgis@gmail.com>
2020-11-29 14:06:20 -0800

    Set up plugin config

--- server/setup.js
+++ server/setup.js
@@ -7,3 +7,28 @@ if(process.env.NODE_LOG_LEVEL){
 }else{
   log.setDefaultLevel("info");
 }
+
+const defaults = {
+  url: 'http://104.131.78.177:8000/treetracker-wallet-api/log'
+  method: 'POST',
+  headers: {},
+  token: '',
+  onUnauthorized: failedToken => {},
+  timeout: 0,
+  interval: 1000,
+  level: 'debug',
+  backoff: {
+    multiplier: 2,
+    jitter: 0.1,
+    limit: 30000,
+  },
+  capacity: 500,
+  stacktrace: {
+    levels: ['warn', 'error'],
+    depth: 3,
+    excess: 0,
+  },
+  timestamp: () => new Date().toISOString(),
+  format: remote.plain,
+};
+apply(loglevel, options)
