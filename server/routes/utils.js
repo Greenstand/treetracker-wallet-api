9fc47d85fb8718c2495237b4cb61cdf05e6502bc
dadiorchen <dadiorchen@outlook.com>
2021-02-25 07:10:31 +0000

    chore: test log.error

--- server/routes/utils.js
+++ server/routes/utils.js
@@ -33,7 +33,7 @@ exports.handlerWrapper = fn =>
   }
 
 exports.errorHandler = (err, req, res, next) => {
-  log.debug("catch error:", err);
+  log.error("catch error:", err);
   if(err instanceof HttpError){
     res.status(err.code).send({
       code: err.code,
