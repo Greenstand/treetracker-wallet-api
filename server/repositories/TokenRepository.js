811ab59239b1b5c2e4c472eddd64798c5860359f
zo-chu <zovut.chudo@gmail.com>
2021-04-06 21:30:43 +0100

    fix more linter errors

--- server/repositories/TokenRepository.js
+++ server/repositories/TokenRepository.js
@@ -1,21 +1,26 @@
-const expect = require("expect-runtime");
+const expect = require('expect-runtime');
 const knex = require('../database/knex');
 const config = require('../../config/config');
-const HttpError = require("../utils/HttpError");
-const BaseRepository = require("./BaseRepository");
-const Session = require("../models/Session");
+const HttpError = require('../utils/HttpError');
+const BaseRepository = require('./BaseRepository');
+const Session = require('../models/Session');
 
-class TokenRepository extends BaseRepository{
-  constructor(session){
-    super("token", session);
-    this._tableName = "token";
+class TokenRepository extends BaseRepository {
+  constructor(session) {
+    super('token', session);
+    this._tableName = 'token';
     this._session = session;
   }
 
-  async getById(id){
-    const result = await this._session.getDB()(this._tableName).where("id", id)
+  async getById(id) {
+    const result = await this._session
+      .getDB()(this._tableName)
+      .where('id', id)
       .first();
-    expect(result,() => new HttpError(404, `can not found token by id:${id}`)).match({
+    expect(
+      result,
+      () => new HttpError(404, `can not found token by id:${id}`),
+    ).match({
       id: expect.any(String),
     });
     return result;
@@ -24,14 +29,14 @@ class TokenRepository extends BaseRepository{
   /*
    * select transaction table by transfer id, return matched tokens
    */
-  async getByTransferId(transferId, limit, offset = 0){
+  async getByTransferId(transferId, limit, offset = 0) {
     return await this._session.getDB().raw(`
       SELECT "token".* FROM "token"
       JOIN "transaction" 
       ON "token".id = "transaction".token_id
       WHERE "transaction".transfer_id = ${transferId}
       LIMIT ${limit}
-      OFFSET ${offset}`)
+      OFFSET ${offset}`);
   }
 }
 
