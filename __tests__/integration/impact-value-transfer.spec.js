b0565db6ab52fe1747b3438f83ee6087704ab2ff
deanchen <antlet2004@gmail.com>
2021-04-24 16:48:01 +0800

    chore: define test for impact value

---
+++ __tests__/integration/impact-value-transfer.spec.js
@@ -0,0 +1,10 @@
+
+describe('Zaven request to send 4 impact value to Meisze', () => {
+
+  beforeEach(async () => {
+  })
+
+  it("Meisze accept the request, then token worth 4 impact should belong to Meisze", () => {
+  });
+
+});
