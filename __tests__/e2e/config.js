3e437e3539ed55fe2dbefdb42dd533f38db6ee02
trunekm <michal.trunek@tipico.com>
2021-06-02 11:44:45 +0200

    fix: adding npm command to actions

--- __tests__/e2e/config.js
+++ __tests__/e2e/config.js
@@ -1,10 +1,10 @@
 require("dotenv").config();
 
-let request = require("supertest")("https://dev-k8s.treetracker.org/wallet");
-if (process.env.ENVIRONMENT === 'dev')
-    request = require("supertest")("https://dev-k8s.treetracker.org/wallet");
-if (process.env.ENVIRONMENT === 'test')
-    request = require("supertest")("https://test-k8s.treetracker.org/wallet");
+const request = require("supertest")(`https://${process.env.ENVIRONMENT}-k8s.treetracker.org/wallet`);
+// if (process.env.ENVIRONMENT === 'dev')
+//    request = require("supertest")("https://dev-k8s.treetracker.org/wallet");
+// if (process.env.ENVIRONMENT === 'test')
+//    request = require("supertest")("https://test-k8s.treetracker.org/wallet");
 // const request = require("supertest")("https://dev-k8s.treetracker.org/wallet");
 const expect = require("chai").expect;
 const responseStatus = require("http-status-codes");
