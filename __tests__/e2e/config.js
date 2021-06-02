9520a3d25247e3882adcae3a9f8002c60f8cafa3
trunekm <michal.trunek@tipico.com>
2021-06-02 09:32:55 +0200

    fix: setting up e2e yml for test env

--- __tests__/e2e/config.js
+++ __tests__/e2e/config.js
@@ -3,6 +3,8 @@ require("dotenv").config();
 let request = require("supertest")("https://dev-k8s.treetracker.org/wallet");
 if (process.env.ENV === 'dev')
     request = require("supertest")("https://dev-k8s.treetracker.org/wallet");
+if (process.env.ENV === 'test')
+    request = require("supertest")("https://test-k8s.treetracker.org/wallet");
 // const request = require("supertest")("https://dev-k8s.treetracker.org/wallet");
 const expect = require("chai").expect;
 const responseStatus = require("http-status-codes");
