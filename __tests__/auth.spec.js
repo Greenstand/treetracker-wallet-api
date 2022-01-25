740d7a0db1f14261eaf94d17e9fe2e191119853b
deanchen <antlet2004@gmail.com>
2022-01-25 16:10:49 +0800

    chore: test

--- __tests__/auth.spec.js
+++ __tests__/auth.spec.js
@@ -54,7 +54,7 @@ describe('Authentication', () => {
   });
 
   // add this to test GET /wallets to optimize the performance
-  it(`[POST /auth] login with using walletB id: ${seed.walletB.id}`, async () => {
+  it.only(`[POST /auth] login with using walletB id: ${seed.walletB.id}`, async () => {
     console.warn("start...");
     let res = await request(server)
       .post('/auth')
