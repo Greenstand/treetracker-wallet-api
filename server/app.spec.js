ca43beda9681d0a19466f594b7cbd1fa059215e2
Olojakpoke Daniel <holodja@hotmail.com>
2022-10-29 23:21:32 +0100

    chore: fix app.spec.js

--- server/app.spec.js
+++ server/app.spec.js
@@ -1,12 +1,14 @@
 const request = require('supertest');
 const { expect } = require('chai');
 const server = require('./app');
+const { version } = require('../package.json');
 
 describe('', () => {
   it('Test header: content-type: application/json', async () => {
     const res = await request(server).get('/');
-    expect(res.statusCode).eq(401);
-    expect(res.body).property('message').eql('Invalid access - no API key');
+    expect(res.statusCode).eq(200);
+    expect(res.body).eql({});
+    expect(res.text).eql(version);
   });
 
   it('Test header: content-type: application/json', async () => {
