28d4b5b94ca1e0c7572e896cbd70d4c816654d5b
Olojakpoke Daniel <holodja@hotmail.com>
2023-03-14 20:10:11 +0100

    chore: fix test

--- server/services/ApiKeyService.spec.js
+++ server/services/ApiKeyService.spec.js
@@ -1,35 +1,36 @@
-const ApiKeyService = require("./ApiKeyService");
-const jestExpect = require("expect");
-const sinon = require("sinon");
-const ApiKeyRepository = require("../repositories/ApiKeyRepository");
-const HttpError = require("../utils/HttpError");
-const Session = require("../models/Session");
+const ApiKeyService = require('./ApiKeyService');
+const jestExpect = require('expect');
+const sinon = require('sinon');
+const ApiKeyRepository = require('../repositories/ApiKeyRepository');
+const HttpError = require('../utils/HttpError');
+const Session = require('../models/Session');
 
-describe("ApiKey", () => {
+describe('ApiKey', () => {
   let apiKey;
 
   before(() => {
     apiKey = new ApiKeyService(new Session());
-  })
+  });
 
-  it("empty key should throw error", async () => {
+  it('empty key should throw error', async () => {
     await jestExpect(async () => {
       await apiKey.check(undefined);
     }).rejects.toThrow(/no API key/);
   });
 
-  it("key which do not exist should throw error", async () => {
-    sinon.stub(ApiKeyRepository.prototype, "getByApiKey").throws(new HttpError(404));
+  it('key which do not exist should throw error', async () => {
+    sinon
+      .stub(ApiKeyRepository.prototype, 'getByApiKey')
+      .throws(new HttpError(404));
     await jestExpect(async () => {
-      await apiKey.check("not_exist");
+      await apiKey.check('not_exist', '');
     }).rejects.toThrow(/Invalid/);
     ApiKeyRepository.prototype.getByApiKey.restore();
   });
 
-  it("good key should pass", async () => {
-    sinon.stub(ApiKeyRepository.prototype, "getByApiKey").returns({});
-    await apiKey.check("not_exist");
+  it('good key should pass', async () => {
+    sinon.stub(ApiKeyRepository.prototype, 'getByApiKey').returns({});
+    await apiKey.check('not_exist', '');
     ApiKeyRepository.prototype.getByApiKey.restore();
   });
-
 });
