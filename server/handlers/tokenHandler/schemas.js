4869b7f8be790d061161d3ea4beb2960ab0f98a9
Pranav Kakaraparti <pkakaraparti@ufl.edu>
2023-08-20 15:17:10 -0500

    fix: remove unused query param validation

--- server/handlers/tokenHandler/schemas.js
+++ server/handlers/tokenHandler/schemas.js
@@ -10,7 +10,6 @@ const tokenGetTransactionsByIdSchema = Joi.object({
   limit: Joi.number().min(1).max(1000).integer().default(1000).required(),
   offset: Joi.number().min(0).integer(),
   id: Joi.string().guid(),
-  transactions: Joi.string(),
 });
 
 module.exports = {
