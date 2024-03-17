e40ba054b316fd1a1febaf5d2daa7965c4e097b8
Edgar Mlowe <mloweedgarvitus@gmail.com>
2024-03-17 15:01:30 +0300

    feat(trust.js): add getAllTrustRelationships function
    Added `getAllTrustRelationships` in trust.js for filtered and paginated trust relationships.

--- server/models/Trust.js
+++ server/models/Trust.js
@@ -50,6 +50,25 @@ class Trust {
   }
 
   /*
+   * Get all trust relationships by filters, setting filter to undefined to allow all data
+   */
+  async getAllTrustRelationships({ state, type, request_type, offset, limit }) {
+    const filter = {
+      and: [],
+    };
+    if (state) {
+      filter.and.push({ state });
+    }
+    if (type) {
+      filter.and.push({ type });
+    }
+    if (request_type) {
+      filter.and.push({ request_type });
+    }
+    return this._trustRepository.getAllByFilter(filter, { offset, limit });
+  }
+
+  /*
    * Get all relationships which has been accepted
    */
   async getTrustRelationshipsTrusted(walletId) {
