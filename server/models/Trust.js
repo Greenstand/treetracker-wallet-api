102e23119eb46dc61f494497e553f584afca0a8b
samwel141 <samwelwilson141@gmail.com>
2024-07-16 14:39:45 +0300

    fix: fix typo

--- server/models/Trust.js
+++ server/models/Trust.js
@@ -15,6 +15,31 @@ class Trust {
     return this._trustRepository.getById(id);
   }
 
+  static getTrustRelationshipFilter({ walletId, state, type, request_type }) {
+    const filter = {
+      and: [
+        {
+          or: [
+            { actor_wallet_id: walletId },
+            { target_wallet_id: walletId },
+            { originator_wallet_id: walletId },
+          ],
+        },
+      ],
+    };
+    if (state) {
+      filter.and.push({ state });
+    }
+    if (type) {
+      filter.and.push({ type });
+    }
+    if (request_type) {
+      filter.and.push({ request_type });
+    }
+
+    return filter;
+  }
+
   /*
    * Get trust relationships by filters, setting filter to undefined to allow all data
    */
@@ -59,35 +84,7 @@ class Trust {
     }
     return this._trustRepository.getByFilter(filter, { offset, limit, sort_by, order });
   }
-
-  /*
-   * Get trust relationships by filters, setting filter to undefined to allow all data
-  //  */
-  // async getTrustRelationships({
-  //   walletId,
-  //   state,
-  //   type,
-  //   request_type,
-  //   offset,
-  //   limit,
-  //   sort_by,
-  //   order,
-  // }) {
-  //   const filter = Trust.getTrustRelationshipFilter({
-  //     walletId,
-  //     state,
-  //     type,
-  //     request_type,
-  //   });
-
-  //   return this._trustRepository.getByFilter(filter, {
-  //     offset,
-  //     limit,
-  //     sort_by,
-  //     order,
-  //   });
-  // }
-
+  
   async getTrustRelationshipsCount({ walletId, state, type, request_type }) {
     const filter = Trust.getTrustRelationshipFilter({
       walletId,
