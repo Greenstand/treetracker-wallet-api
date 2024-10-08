21388b9f3ce5fbba68977b04cbc1b1f6c393a78d
Olojakpoke Daniel <holodja@hotmail.com>
2024-10-08 17:08:31 +0100

    fix: trigger deployment

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ require('dotenv').config({ path: `.env.${process.env.NODE_ENV}` });
 const log = require('loglevel');
 // setup log level
 require('./setup');
+
 const app = require('./app');
 
 const port = process.env.NODE_PORT || 3006;
