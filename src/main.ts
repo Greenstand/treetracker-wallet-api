6686ae9ef61ad1297b06835ea77cd38868d1420f
yunchipang <yunchipang@gmail.com>
2025-01-06 11:32:05 -0800

    feat: add global exception handler

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './modules/app/app.module';
+import { GlobalExceptionFilter } from './common/filters/global-exception.filter';
 
 const PORT = process.env.PORT || 3006;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new GlobalExceptionFilter());
   await app.listen(PORT);
 }
 
