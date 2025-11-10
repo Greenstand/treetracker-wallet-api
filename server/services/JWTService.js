80b15e8c4e148a0666769ed7fbdcf436446c3ce4
Nachiket Kamod <nkamod@uccs.edu>
2025-11-10 08:07:10 -0600

    feat(keycloak): verify keycloak and allow access to api

--- server/services/JWTService.js
+++ server/services/JWTService.js
@@ -4,6 +4,7 @@
  */
 const JWTTools = require('jsonwebtoken');
 const log = require('loglevel');
+const jwksClient = require('jwks-rsa');
 const HttpError = require('../utils/HttpError');
 
 // PRIVATE and PUBLIC key
@@ -22,12 +23,25 @@ const verifyOptions = {
   algorithms: ['RS256'],
 };
 
+const client = jwksClient({
+  jwksUri: `${process.env.PRIVATE_KEYCLOAK_BASE_URL}/realms/${process.env.PRIVATE_KEYCLOAK_REALM}/protocol/openid-connect/certs`
+});
+
+const fetchPublicKey = async (header) => {
+  return new Promise((resolve, reject) => {
+      client.getSigningKey(header.kid, (err, key) => {
+          if (err) return reject(err);
+          return resolve(key.publicKey || key.rsaPublicKey);
+      });
+  });
+};
+
 class JWTService {
   static sign(payload) {
     return JWTTools.sign(payload, privateKEY, signingOptions);
   }
 
-  static verify(authorization) {
+  static async verify(authorization) {
     if (!authorization) {
       throw new HttpError(
         401,
@@ -39,21 +53,46 @@ class JWTService {
     const token = tokenArray[1];
     let result;
     if (token) {
-      // Decode the token
-      JWTTools.verify(token, publicKEY, verifyOptions, (err, decod) => {
-        if (err || tokenArray[0] !== 'Bearer') {
-          log.debug(err);
-          throw new HttpError(401, 'ERROR: Authentication, token not verified');
+
+      const decoded = JWTTools.decode(token, { complete: true });
+
+      if (decoded.payload.iss === `${process.env.PRIVATE_KEYCLOAK_BASE_URL}/realms/${process.env.PRIVATE_KEYCLOAK_REALM}`) {
+        
+        try {
+          const signingKey = await fetchPublicKey(decoded.header);
+  
+          result = JWTTools.verify(token, signingKey, {
+            issuer: `${process.env.PRIVATE_KEYCLOAK_BASE_URL}/realms/${process.env.PRIVATE_KEYCLOAK_REALM}`,
+            algorithms: ['RS256']
+          });
+
+          // Unlike server token, keycloak token does not have id property, so we temporarily use sub property
+          // as a workaround to get the user id
+          result.id = result.sub;
+          return result;
+        } catch (error) {
+          log.debug('Keycloak token verification error', error);
+          throw new HttpError(401, `Unauthorized: ${error.message}`);
         }
-        result = decod;
-        if (!result.id)
-          throw new HttpError(
-            401,
-            'ERROR: Authentication, invalid token received',
-          );
-      });
+      } else {
+          
+        // Decode the token
+        JWTTools.verify(token, publicKEY, verifyOptions, (err, decod) => {
+          if (err || tokenArray[0] !== 'Bearer') {
+            log.debug(err);
+            throw new HttpError(401, 'ERROR: Authentication, token not verified');
+          }
+          result = decod;
+          if (!result.id)
+            throw new HttpError(
+              401,
+              'ERROR: Authentication, invalid token received',
+            );
+        });        
+      }
+
     } else {
-      throw new HttpError(401, 'ERROR: Authentication, token not verified');
+      throw new HttpError(401, 'ERROR: Authentication, token not provided');
     }
     return result;
   }
--- server/services/JWTService.spec.js
+++ server/services/JWTService.spec.js
@@ -1,12 +1,40 @@
 const { expect } = require('chai');
+require('../setup'); // Configure loglevel based on NODE_LOG_LEVEL
+const log = require('loglevel');
+const axios = require('axios');
 const JWTService = require('./JWTService');
 
 describe('JWTService', () => {
-  it('signed payload should be able to be verified', () => {
+  it('signed payload should be able to be verified', async () => {
     const payload = { id: 1 };
     const token = JWTService.sign(payload);
     expect(token).match(/\S+/);
-    const result = JWTService.verify(`Bearer ${token}`);
+    const result = await JWTService.verify(`Bearer ${token}`);
     expect(result).property('id').eq(1);
+  });
+
+  it('Keycloak token should be able to be verified', async () => {
+    
+    // White we can use keycloak token from env file, its a good idea to use fresh token for testing
+    // const keycloakUserToken = process.env.KEYCLOAK_USER_TOKEN || '';
+
+    const params = new URLSearchParams();
+    params.append('grant_type', 'client_credentials');
+    params.append('client_id', process.env.PRIVATE_KEYCLOAK_CLIENT_ID);
+    params.append('client_secret', process.env.PRIVATE_KEYCLOAK_CLIENT_SECRET);
+
+    const headers = {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    };
+
+    try {
+      const response = await axios.post(`${process.env.PRIVATE_KEYCLOAK_BASE_URL}/realms/${process.env.PRIVATE_KEYCLOAK_REALM}/protocol/openid-connect/token`, params, { headers });
+      const keycloakUserToken = response.data.access_token;
+      const result = await JWTService.verify(`Bearer ${keycloakUserToken}`);
+      expect(result).property('preferred_username').eq(`service-account-${process.env.PRIVATE_KEYCLOAK_CLIENT_ID}`);
+    } catch (error) {
+      log.debug(error);
+      throw error;
+    }
   });
 });
